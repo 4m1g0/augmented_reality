c79c082bf0d6d75fdd7f1498b022d90c86927e98
4m1g0 <dev.4m1g0@gmail.com>
2017-04-20 18:09:14 +0200

    Add example of full inmersive full screen on display overly with back background

---
+++ OverlayTest/app/src/main/java/com/a4m1g0/testapp/overlaytest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.a4m1g0.testapp.overlaytest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(0x80000000);
+
+        setContentView(R.layout.activity_main);
+    }
+}
